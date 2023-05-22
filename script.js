2a82095466a95c346e615f9b64fdd9468a5b1e36
hafiz1379 <hafizrasa1379@gmail.com>
2023-05-22 14:22:09 +0430

    Add keydown function and event

--- script.js
+++ script.js
@@ -2,26 +2,44 @@ let displayValue = "";
 
 function appendNumber(number) {
   displayValue += number;
-  document.getElementById("display").value = displayValue;
+  updateDisplay();
 }
 
 function performOperation(operator) {
   displayValue += operator;
-  document.getElementById("display").value = displayValue;
+  updateDisplay();
 }
 
 function clearDisplay() {
   displayValue = "";
-  document.getElementById("display").value = displayValue;
+  updateDisplay();
 }
 
 function calculateResult() {
   try {
     const result = eval(displayValue);
     displayValue = result.toString();
-    document.getElementById("display").value = displayValue;
+    updateDisplay();
   } catch (error) {
     displayValue = "";
     document.getElementById("display").value = "Error";
   }
 }
+
+function updateDisplay() {
+  document.getElementById("display").value = displayValue;
+}
+
+document.addEventListener("keydown", function(event) {
+  const key = event.key;
+
+  if (/\d/.test(key)) {
+    appendNumber(key);
+  } else if (key === "+" || key === "-" || key === "*" || key === "/") {
+    performOperation(key);
+  } else if (key === "Enter") {
+    calculateResult();
+  } else if (key === "Escape") {
+    clearDisplay();
+  }
+});
