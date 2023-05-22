d47b2a27ccc8bcfcae829b069f7875b45f7a8bbc
hafiz1379 <hafizrasa1379@gmail.com>
2023-05-22 14:45:55 +0430

    Add new functons for new operations

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 let displayValue = "";
+let isResultShown = false;
 
 function appendNumber(number) {
+  if (isResultShown) {
+    displayValue = "";
+    isResultShown = false;
+  }
   displayValue += number;
   updateDisplay();
 }
 
+function appendDecimal() {
+  if (isResultShown) {
+    displayValue = "0.";
+    isResultShown = false;
+  } else if (!displayValue.includes(".")) {
+    displayValue += ".";
+  }
+  updateDisplay();
+}
+
 function performOperation(operator) {
+  if (isResultShown) {
+    isResultShown = false;
+  }
   displayValue += operator;
   updateDisplay();
 }
@@ -16,14 +34,20 @@ function clearDisplay() {
 }
 
 function deleteDigit() {
-  displayValue = displayValue.slice(0, -1);
-  updateDisplay();
+  if (isResultShown) {
+    clearDisplay();
+    isResultShown = false;
+  } else {
+    displayValue = displayValue.slice(0, -1);
+    updateDisplay();
+  }
 }
 
 function calculateResult() {
   try {
     const result = eval(displayValue);
     displayValue = result.toString();
+    isResultShown = true;
     updateDisplay();
   } catch (error) {
     displayValue = "";
@@ -40,15 +64,13 @@ document.addEventListener("keydown", function(event) {
 
   if (/\d/.test(key)) {
     appendNumber(key);
+  } else if (key === "." && !displayValue.includes(".")) {
+    appendDecimal();
   } else if (key === "+" || key === "-" || key === "*" || key === "/") {
     performOperation(key);
   } else if (key === "Enter") {
     calculateResult();
   } else if (key === "Backspace") {
-    if (displayValue.length === 0) {
-      clearDisplay();
-    } else {
-      deleteDigit();
-    }
+    deleteDigit();
   }
 });
